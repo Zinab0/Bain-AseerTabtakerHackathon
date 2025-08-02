989c94025c0efb45ff76620bd906d101092e7141
Hadeel Althbiti <hadeelalthbiti@gmail.com>
2025-08-02 09:19:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+const firebaseConfig: FirebaseOptions = { 
+    apiKey: "AIzaSyDUhK_nRhFBSl3xfOzzKW-jeiDyr9rVFnI", 
+    authDomain: "asir-connect.firebaseapp.com", 
+    projectId: "asir-connect", 
+    storageBucket: "asir-connect.appspot.com", 
+    messagingSenderId: "78913698870", 
+    appId: "1:78913698870:web:dc305996d49bd3a364ac97", 
 };
+
 
 // Initialize Firebase
 let app;
