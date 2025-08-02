54fded549b73ffe10d0e059d83ef0766b30d1b8d
Hadeel Althbiti <hadeelalthbiti@gmail.com>
2025-08-02 08:41:30 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,11 +7,11 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "asir-connect.firebaseapp.com",
-  projectId: "asir-connect",
-  storageBucket: "asir-connect.firebasestorage.app",
-  messagingSenderId: "78913698870",
-  appId: "1:78913698870:web:dc305996d49bd3a364ac97",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -27,3 +27,4 @@ const storage = getStorage(app);
 const auth = getAuth(app);
 
 export { app, db, storage, auth };
+
