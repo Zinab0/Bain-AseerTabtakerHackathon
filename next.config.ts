b357e55816944e7733ea356c475fc59a55178336
Zainab Alsaggaf <zinabalsaggaf@gmail.com>
2025-08-02 08:56:30 +0000

    I’m working on a Next.js project with Firebase Authentication, but I kee

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyDUhK_nRhFBSl3xfOzzKW-jeiDyr9rVFnI",
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "asir-connect.firebaseapp.com",
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: "asir-connect",
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "asir-connect.firebasestorage.app",
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "78913698870",
+    NEXT_PUBLIC_FIREBASE_APP_ID: "1:78913698870:web:dc305996d49bd3a364ac97",
+  },
   images: {
     remotePatterns: [
       {
